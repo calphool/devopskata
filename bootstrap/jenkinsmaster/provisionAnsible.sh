e94d575a30c6836286e47c404f4f5953518c3a6f
calphool <jrounceville@hotmail.com>
2016-07-17 02:14:14 -0500

    fixed pathing in provisionAnsible.sh

--- bootstrap/jenkinsmaster/provisionAnsible.sh
+++ bootstrap/jenkinsmaster/provisionAnsible.sh
@@ -25,5 +25,5 @@ sudo echo '[jenkinsmaster]' | sudo tee --append /etc/ansible/hosts
 sudo echo 'localhost' | sudo tee --append /etc/ansible/hosts
 sudo ansible jenkinsmaster -m ping
 sudo ansible-galaxy install geerlingguy.jenkins
-sudo ansible-playbook ~/devopskata/bootstrap/jenkinsmaster/startJenkins.yml
-sudo tar zxf ~/devopskata/bootstrap/jenkinsmaster/jenkins_state.tar.gz -C /var/lib/jenkins
+sudo ansible-playbook /home/ec2-user/devopskata/bootstrap/jenkinsmaster/startJenkins.yml
+sudo tar zxf /home/ec2-user/devopskata/bootstrap/jenkinsmaster/jenkins_state.tar.gz -C /var/lib/jenkins
