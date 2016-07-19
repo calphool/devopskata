6b47ec871ae5726ffe35cb9e65abc2d92565aec7
calphool <jrounceville@hotmail.com>
2016-07-19 01:15:00 -0500

    clean up scripts
    Former-commit-id: a5c15e3e39d4901d839317f3872f27cf15269b67

--- bootstrap/jenkinsmaster/updateGithubWebhook.sh
+++ bootstrap/jenkinsmaster/updateGithubWebhook.sh
@@ -6,7 +6,7 @@ echo "repo name        : $2"
 echo "github user      : $3"
 echo "github pwd       : ********"
 echo " "
-cp githubWebhook.txt githubWebhook.bak
+cp githubWebhook.template githubWebhook.txt
 sed -i '' "s/EC2EXTERNALNAME/$(echo $1 | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')/g" githubWebhook.txt
-curl -v -i -u "$3:$4" -X POST --data-binary @githubWebhook.txt https://api.github.com/repos/$3/$2/hooks
-mv githubWebhook.bak githubWebhook.txt
+curl -i -u "$3:$4" -X POST --data-binary @githubWebhook.txt https://api.github.com/repos/$3/$2/hooks
+rm githubWebhook.txt
