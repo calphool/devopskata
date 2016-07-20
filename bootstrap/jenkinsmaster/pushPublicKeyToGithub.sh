4997537523f84413ad41d36ee94ad92e67803193
calphool <jrounceville@hotmail.com>
2016-07-20 01:31:59 -0500

    public key push
    Former-commit-id: b171e3b117407752d1b52943fbd6a9ee8fef5438

--- bootstrap/jenkinsmaster/pushPublicKeyToGithub.sh
+++ bootstrap/jenkinsmaster/pushPublicKeyToGithub.sh
@@ -1,1 +1,8 @@
-echo Code that pushes ./id_rsa to $1, using $2:$3 goes here...
+echo Code that pushes ./id_rsa.pub to $1, using $2:***** goes here...
+
+keydata=$(cat ./id_rsa.pub)
+str1='{"title": "ec2key", "key": "'
+str2=$keydata'", "read_only": true }'
+str3=$str1$str2
+str4=\'$str3\'
+curl -X POST -u "$2:$3" $str4
