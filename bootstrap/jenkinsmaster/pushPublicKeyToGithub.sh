590936c3146198d2602d7d2e7403bffe5b2566f2
calphool <jrounceville@hotmail.com>
2016-07-20 02:53:14 -0500

    fix key title

--- bootstrap/jenkinsmaster/pushPublicKeyToGithub.sh
+++ bootstrap/jenkinsmaster/pushPublicKeyToGithub.sh
@@ -1,9 +1,10 @@
 echo Code that pushes ./id_rsa.pub to $1, using $2:***** goes here...
 
 keydata=$(cat ./id_rsa.pub)
-str1='{"title": "key-$4", "key": "'
-str2=$keydata'", "read_only": true }'
-str3=$str1$str2
+str1='{"title": "key-'
+str1a=$str1$4"\", \"key\": "
+str2=\"$keydata'", "read_only": true }'
+str3=$str1a$str2
 echo $str3 > tmp.txt
 curl -H "Content-Type: application/json" -v -X POST -u $2:$3 https://api.github.com/repos/$2/$1/keys --data @tmp.txt
-rm tmp.txt 2> /dev/null
+#rm tmp.txt 2> /dev/null
