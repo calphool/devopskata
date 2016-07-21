5ddb92fb17284ad91c1ebf036eb034185348ab5d
calphool <jrounceville@hotmail.com>
2016-07-20 19:39:03 -0500

    missed a provision script
    Former-commit-id: f56609492cbd167c6e7e51d07a48f6894d5b1672

--- bootstrap/jenkinsmaster/pushPublicKeyToGithub.sh
+++ bootstrap/jenkinsmaster/pushPublicKeyToGithub.sh
@@ -9,4 +9,4 @@ str2=\"$keydata'", "read_only": true }'
 str3=$str1a$str2
 echo $str3 > tmp.txt
 curl -H "Content-Type: application/json" -v -X POST -u $2:$3 https://api.github.com/repos/$2/$1/keys --data @tmp.txt
-#rm tmp.txt 2> /dev/null
+rm tmp.txt 2> /dev/null
