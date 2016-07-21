000b7e7e0e24632e16f40943d1d52e0b43525684
calphool <jrounceville@hotmail.com>
2016-07-20 23:23:20 -0500

    start tomcat built out

--- bootstrap/jenkinsmaster/setupTargetServer.sh
+++ bootstrap/jenkinsmaster/setupTargetServer.sh
@@ -5,3 +5,23 @@ sudo yum -y install wget
 sudo yum -y install git
 sudo yum -y install java
 java -version
+cd ~
+sudo yum -y install python
+sudo yum -y install python-setuptools
+sudo yum -y install gcc
+sudo yum -y install python-devel
+sudo yum -y install libffi-devel
+sudo yum -y install openssl-devel
+sudo yum -y install python-pip
+sudo yum -y install nano
+sudo pip install --upgrade pip
+sudo pip install --upgrade setuptools
+sudo pip install ansible
+hash -r
+sudo mkdir -p /etc/ansible
+sudo echo 'localhost ansible_connection=local' | sudo tee --append /etc/ansible/hosts
+sudo echo '[buildserver]' | sudo tee --append /etc/ansible/hosts
+sudo echo 'localhost' | sudo tee --append /etc/ansible/hosts
+sudo ansible buildserver -m ping
+ansible-galaxy install osct.tomcat-8
+sudo ansible-playbook /home/ec2-user/devopskata/bootstrap/buildserver/startTomcat.yml
