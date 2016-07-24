fcc739fcf9a34a33807fe930be2cfcaa0c3f2d38
calphool <jrounceville@hotmail.com>
2016-07-24 14:02:14 -0500

    add s3fs role install

--- bootstrap/jenkinsmaster/provisionAnsibleOnJenkinsMaster.sh
+++ bootstrap/jenkinsmaster/provisionAnsibleOnJenkinsMaster.sh
@@ -33,3 +33,4 @@ sudo ansible-galaxy install geerlingguy.jenkins
 sudo tar zxf /perm/jenkins_state.tar.gz -C /
 sudo ansible-playbook /home/ec2-user/devopskata/bootstrap/jenkinsmaster/startJenkins.yml
 sudo /etc/init.d/jenkins restart
+sudo ansible-galaxy install calphool.s3fs
