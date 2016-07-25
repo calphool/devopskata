586f30cdc5d563ad46a5aec72e213b377974b9bc
calphool <jrounceville@hotmail.com>
2016-07-24 22:32:06 -0500

    bug in provision script

--- bootstrap/jenkinsmaster/provisionAnsibleOnJenkinsMaster.sh
+++ bootstrap/jenkinsmaster/provisionAnsibleOnJenkinsMaster.sh
@@ -36,7 +36,7 @@ sudo /etc/init.d/jenkins restart
 sudo ansible-galaxy install calphool.s3fs
 sudo ansible-playbook /home/ec2-user/devopskata/bootstrap/jenkinsmaster/startS3fs.yml
 echo 'user_allow_other' | sudo tee --append /etc/fuse.conf
-sudo umount -f /home/ec2-user/s3 2> 
+sudo umount -f /home/ec2-user/s3 
 sudo rm -rf /home/ec2-user/s3 
 sudo mkdir -p /home/ec2-user/s3
 sudo s3fs calphoolbucket /home/ec2-user/s3 -o passwd_file=/home/ec2-user/q -o allow_other
