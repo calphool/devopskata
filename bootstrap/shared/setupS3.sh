0f697931d46a412aac4172024a5f6bc97f1bb99b
calphool <jrounceville@hotmail.com>
2016-07-26 11:21:12 -0500

    silence umount errors

--- bootstrap/shared/setupS3.sh
+++ bootstrap/shared/setupS3.sh
@@ -1,7 +1,7 @@
 sudo ansible-galaxy install calphool.s3fs
 sudo ansible-playbook /home/ec2-user/devopskata/bootstrap/$1/startS3fs.yml
 echo 'user_allow_other' | sudo tee --append /etc/fuse.conf
-sudo umount -f /home/ec2-user/s3 
+sudo umount -f /home/ec2-user/s3 2> /dev/null
 sudo rm -rf /home/ec2-user/s3 
 sudo mkdir -p /home/ec2-user/s3
 sudo s3fs calphoolbucket /home/ec2-user/s3 -o passwd_file=/home/ec2-user/q -o allow_other
